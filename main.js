9f336c6dd865ebbff7af3b11a7295a1fac83959d
gabbzin <cgabrielpimentacruz111@gmail.com>
2024-04-06 09:11:38 -0300

    Adição de recolher o elemento do botão
    os valores dos botões agora são recolhidos

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // Recolhendo todos os botões
+var botoes = document.querySelectorAll('.botao');
+var barra = document.querySelector('.barra')
+
+// Adicionando um evento de clique a cada botão
+botoes.forEach(function(botao) {
+  botao.addEventListener('click', function() {
+    // Obtendo o valor do botão clicado
+    var valorBotao = this.value;
+
+    barra.innerText = valorBotao;
+  });
+});
