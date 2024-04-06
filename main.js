f753ca598b2e61f7957d52be8593da681d7654e4
gabbzin <cgabrielpimentacruz111@gmail.com>
2024-04-06 09:32:28 -0300

    Mudanças

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-// Recolhendo todos os botões
+// Recolhendo todos os botões e a barra
 var botoes = document.querySelectorAll('.botao');
-var barra = document.querySelector('.barra')
+var limpar = document.querySelector('#limpar');
+var barra = document.querySelector('.barra');
 
 // Adicionando um evento de clique a cada botão
 botoes.forEach(function(botao) {
   botao.addEventListener('click', function() {
     // Obtendo o valor do botão clicado
     var valorBotao = this.value;
-
-    barra.innerText = valorBotao;
+    barra.innerText += valorBotao;
   });
 });
+
+// Função de limpar
+/*
+limpar.forEach(function(limparTudo) {
+    limparTudo.addEventListener('click', function() {
+        barra.innerHTML = "0";
+    });
+});
+*/
